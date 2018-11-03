8ce0fd7805d154a2ac2829ad8e2475ed871dfb4f
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-03 22:56:29 +0100

    feat: implement cookie-session

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 
 const keys = require('./config/keys')
+
+// Models
+require('./models/User')
+
+// Services
 require('./services/passport')
 
+// Connect to MongoDB
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true }
@@ -11,8 +19,18 @@ mongoose.connect(
 
 const app = express()
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Routes
 require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT)
